1c54da034c008c2e17429c73893d289844a23feb
Raquel Oliveira <ana.santos@vibetecnologia.com>
2024-03-08 14:15:54 -0300

    inclusao da lista de contatos mockada com erro no ngFor

--- src/app/contatos-listagem/contatos-listagem.component.ts
+++ src/app/contatos-listagem/contatos-listagem.component.ts
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './contatos-listagem.component.html',
   styleUrl: './contatos-listagem.component.css'
 })
+
 export class ContatosListagemComponent {
+
+  contatos = [
+    { id: 1, nome: 'Alexandre', email: 'alexandre@email.com'},
+    { id: 2, nome: 'Thiago', email: 'thiago@email.com'},
+  ];
+  
+  constructor() {}
+
+  ngOnInit() {
+    
+  }
 
 }
