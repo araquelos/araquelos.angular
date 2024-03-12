96285d5e588f0798376a586406fc4736ba845615
Raquel Oliveira <ana.santos@vibetecnologia.com>
2024-03-12 12:39:30 -0300

    configuracao inicial e criacao do projeto vendas

---
+++ vendas/src/main/java/io/github/araquelos/VendasApplication.java
@@ -0,0 +1,10 @@
+package io.github.araquelos;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+    }
+}
