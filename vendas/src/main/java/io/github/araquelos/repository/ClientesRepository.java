9b1b0a2bb50468827f4be07e617447a04e28b88a
Raquel VIBE <ana.santos@vibetecnologia.com>
2024-03-13 10:54:46 -0300

    criacao do servico repositorio e modelo para clientes com injecao de dependencia

---
+++ vendas/src/main/java/io/github/araquelos/repository/ClientesRepository.java
@@ -0,0 +1,11 @@
+package io.github.araquelos.repository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+
+    public void salvar (Cliente cliente) {
+        //Acessa a base e salva o cliente
+    }
+
+}
