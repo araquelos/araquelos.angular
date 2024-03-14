410b55053689a6e2294a31b7fea67e2f5fb8229c
Raquel Oliveira <ana.santos@vibetecnologia.com>
2024-03-14 17:31:33 -0300

    criacao das classes servico repositorio e application properties com injecao de dependencia

--- vendas/src/main/java/io/github/araquelos/TestConfiguration.java
+++ vendas/src/main/java/io/github/araquelos/TestConfiguration.java
@@ -1,13 +1,7 @@
 package io.github.araquelos;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TestConfiguration {
 
-    @Bean(name="applicationName")
-    public String applicationName() {
-        return "Sistema de Vendas";
-    }
 }
---
+++ vendas/src/main/java/io/github/araquelos/service/ClientesService.java
@@ -0,0 +1,26 @@
+package io.github.araquelos.service;
+
+import io.github.araquelos.model.Cliente;
+import io.github.araquelos.repository.ClientesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientesService {
+
+    private ClientesRepository repository;
+
+    @Autowired
+    public ClientesService (ClientesRepository repository) {
+        this.repository = repository;
+    }
+    public void salvarCliente(Cliente cliente) {
+        validarCliente(cliente);
+        ClientesRepository clientesRepository = new ClientesRepository();
+        clientesRepository.salvar(cliente);
+    }
+
+    public void validarCliente(Cliente cliente) {
+        //aplica validações
+    }
+}
