59c6452cd96cacd592e75b3665c04ccf406ada1d
Raquel Oliveira <ana.santos@vibetecnologia.com>
2024-03-14 16:25:46 -0300

    classe de configuracao @Configuration e objeto configuravel @Bean

--- vendas/src/main/java/io/github/araquelos/VendasApplication.java
+++ vendas/src/main/java/io/github/araquelos/VendasApplication.java
@@ -1,4 +1,6 @@
 package io.github.araquelos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendasApplication {
 
+    @Autowired
+    @Qualifier("applicationName")
+    private String applicationName;
+
     @GetMapping("/hello")
     public String helloWorld() {
-        return "Hello World";
+        return applicationName;
     }
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
