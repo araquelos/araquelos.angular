fce1079756a0369f4e58a5772dcb84693f9a2752
Raquel Oliveira <ana.santos@vibetecnologia.com>
2024-03-22 15:21:57 -0300

    layout de formulario de clientes

--- clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../cliente';
 
 @Component({
   selector: 'app-clientes-form',
@@ -9,4 +10,5 @@ import { Component } from '@angular/core';
 })
 export class ClientesFormComponent {
 
+  cliente!: Cliente;
 }
